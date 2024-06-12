dd7ba462d2007447084ab038724175c845db5349
CodeReactCrack <codereactcrack@gmail.com>
2024-06-12 12:56:26 +0530

    User Context implementaion(WP)

--- Blabber/src/components/Auth/Login/LoginGoogle.jsx
+++ Blabber/src/components/Auth/Login/LoginGoogle.jsx
@@ -1,12 +1,19 @@
 import { signInWithPopup } from 'firebase/auth'
-import React from 'react'
+import React, { useContext } from 'react'
 import { auth, provider } from '../../../services/firebase'
+import UserContext from '../../../context/AuthContext/UserContext'
+import {useNavigate} from 'react-router-dom'
 
 const LoginGoogle = () => {
+
+  const {setCurrentUser} = useContext(UserContext);
+  const naviagte = useNavigate();
 
   async function googleLoginHandler(){
     try {
       const user = await signInWithPopup(auth,provider);
+      setCurrentUser(user);
+      naviagte('/chat-room')
       console.log(user);
     } catch (error) {
      console.log(error.message); 
---
+++ Blabber/src/context/AuthContext/UserContextProvider.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react"
+import UserContext from "./UserContext"
+
+const UserContextProvider = ({children}) =>{
+    
+    const [currentUser,setCurrentUser] = useState(null);
+
+    return (
+        <UserContext.Provider value={{currentUser,setCurrentUser}}>
+            {children}
+        </UserContext.Provider>
+    )
+}
+
+export default UserContextProvider
