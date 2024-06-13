86d380f8088e9a95b05ea8273ed25f81509ba6a5
CodeReactCrack <codereactcrack@gmail.com>
2024-06-13 15:13:04 +0530

    Protected Routes

--- Blabber/src/App.jsx
+++ Blabber/src/App.jsx
@@ -4,17 +4,22 @@ import LoginPage from './components/Auth/Login/LoginPage'
 import RegisterPage from './components/Auth/Register/RegisterPage'
 import ChatRoom from './components/Chat/ChatRoom';
 import Terms from './pages/Terms'
+import { useContext } from 'react';
+import UserContext from './context/AuthContext/UserContext';
 
 function App() {
+
+  const {currentUser} = useContext(UserContext);
 
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route >
         <Route path='/' element ={<LoginPage />} />
         <Route path='/sign-up' element ={<RegisterPage />} />
-        <Route path='/chat-room' element ={<ChatRoom />} />
+        {currentUser && <Route path='/chat-room' element ={<ChatRoom />} />}
         <Route path='/terms' element ={<Terms/>} />
         <Route path='/privacy' element ={<Terms/>} />
+        <Route path='*' element={<div>404! BAD GATEWAY</div>} />
       </Route>
     )
   )
