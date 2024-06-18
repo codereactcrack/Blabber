a17dab0de271e924e7092c7679e974a3dea60af4
CodeReactCrack <codereactcrack@gmail.com>
2024-06-18 18:06:38 +0530

    FIX --Proted Route Correct way

--- Blabber/src/App.jsx
+++ Blabber/src/App.jsx
@@ -3,28 +3,24 @@ import './App.css';
 import LoginPage from './components/Auth/Login/LoginPage'
 import RegisterPage from './components/Auth/Register/RegisterPage'
 import Terms from './pages/Terms'
-import { useContext } from 'react';
-import UserContext from './context/AuthContext/UserContext';
 import MainScreen from './components/MainScreen/MainScreen';
 import ChannelTemplate from './components/Channels/ChannelTemplate';
 import ChatTemplate from './components/Chats/ChatTemplate';
 import Profile from './components/Profile/Profile';
+import ProtectedRoute from './components/Auth/ProtectedRoute/ProtectedRoute';
 
 function App() {
-
-  const {currentUser} = useContext(UserContext);
 
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route >
         <Route path='/' element ={<LoginPage />} />
         <Route path='/sign-up' element ={<RegisterPage />} />
-        {currentUser && currentUser.email &&
-          <Route path='/MainScreen' element ={<MainScreen />}> 
+        <Route path='/MainScreen' element ={<ProtectedRoute element={<MainScreen />} /> }> 
             <Route path='chat' element={<ChatTemplate />} />
             <Route path='channels' element={<ChannelTemplate />} />
             <Route path='profile' element={<Profile />} />
-          </Route>}
+        </Route>
         <Route path='/terms' element ={<Terms/>} />
         <Route path='/privacy' element ={<Terms/>} />
         <Route path='*' element={<div>404! BAD GATEWAY</div>} />
